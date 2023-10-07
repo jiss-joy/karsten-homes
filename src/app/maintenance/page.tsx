21ff0f6b6fbeec8fe26dc902e2416297c5645b98
Jiss Joy <60382278+jiss-joy@users.noreply.github.com>
2023-10-07 15:29:38 +0530

    Create maintenance page

--- src/app/maintenance/page.tsx
+++ src/app/maintenance/page.tsx
@@ -1,7 +1,22 @@
+import Image from 'next/image'
+import maintenance from '/public/images/maintenance.png'
+import { spicyRice } from '@/styles/fonts'
+
 const MaintenancePage = () => {
   return (
-    <div className="w-full h-screen flex justify-center content-end">
-      Maintenance Page
+    <div className="flex h-screen w-full flex-col items-center justify-center px-8">
+      <Image src={maintenance} alt="Maintenance Image" className="w-4/5" />
+      <center>
+        <div className="py-8">
+          <h1 className={`${spicyRice.className} text-3xl`}>Oops!</h1>
+          <h2 className="py-3 text-xl font-bold">We are under maintenance.</h2>
+          <p className="w-64 text-sm">
+            Apologies for the inconvenience, but we&apos;re sprucing things up
+            on our website. We&apos;ll be back online shortly. Thank you for
+            your patience!
+          </p>
+        </div>
+      </center>
     </div>
   )
 }
