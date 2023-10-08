e8303c26e78c2c742d1e6473082a7bb35a8e2a41
Jiss Joy <60382278+jiss-joy@users.noreply.github.com>
2023-10-08 21:22:40 +0530

    add responsiveness

--- src/app/maintenance/page.tsx
+++ src/app/maintenance/page.tsx
@@ -4,19 +4,23 @@ import { spicyRice } from '@/styles/fonts'
 
 const MaintenancePage = () => {
   return (
-    <div className="flex h-screen w-full flex-col items-center justify-center px-8">
-      <Image src={maintenance} alt="Maintenance Image" className="w-4/5" />
-      <center>
-        <div className="py-8">
-          <h1 className={`${spicyRice.className} text-3xl`}>Oops!</h1>
-          <h2 className="py-3 text-xl font-bold">We are under maintenance.</h2>
-          <p className="w-64 text-sm">
-            Apologies for the inconvenience, but we&apos;re sprucing things up
-            on our website. We&apos;ll be back online shortly. Thank you for
-            your patience!
-          </p>
-        </div>
-      </center>
+    <div className="flex h-screen w-full flex-col items-center justify-center px-6 md:flex-row md:items-center ">
+      <Image
+        src={maintenance}
+        alt="Maintenance Image"
+        className="w-4/5 sm:w-3/5 md:w-2/6 lg:w-2/5"
+      />
+      <div className="flex flex-col items-center px-16 py-8 md:w-3/5 md:items-start md:ps-16  lg:w-2/5">
+        <h1 className={`${spicyRice.className} text-3xl md:text-5xl`}>Oops!</h1>
+        <h2 className="sm:text-md py-3 text-xl font-bold  md:text-2xl lg:text-3xl">
+          We are under maintenance.
+        </h2>
+        <p className="sm:text-md text-center text-sm sm:w-3/5 md:w-full md:text-start md:text-lg">
+          Apologies for the inconvenience, but we&apos;re sprucing things up on
+          our website. We&apos;ll be back online shortly. Thank you for your
+          patience!
+        </p>
+      </div>
     </div>
   )
 }
