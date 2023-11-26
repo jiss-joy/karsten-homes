281d689e45962b359e212ccf41af6785c83bafbc
Jiss Joy <60382278+jiss-joy@users.noreply.github.com>
2023-11-26 19:16:09 +0900

    fallback settings in next config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  distDir: 'build',
   async redirects() {
     return [
       {
         source: '/',
         destination: '/maintenance',
         permanent: false,
+      },
+      {
+        source: '/**',
+        destination: '/not-found',
+        permanent: true,
       },
     ]
   },
